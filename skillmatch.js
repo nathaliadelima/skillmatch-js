cdd08081474b85b0100df1919bd1f6a8e3724a07
nathaliadelima <nathaliafollow@gmail.com>
2026-05-16 22:28:37 -0300

    feat
    Estrutura inicial do projeto + dados do candidato e vagas

---
+++ skillmatch.js
@@ -0,0 +1,33 @@
+const candidato = {
+    nome: 'Nathalia',
+    area: 'Front-end',
+    habilidades: ['JavaScript', 'Notion', 'Landing Pages', 'Lógica de programação', 'GitHub'],
+    experienciaMeses: 3,
+}
+
+const vagas = [
+{
+    id: 1,
+    empresa: 'TechStart',
+    cargo: 'Desenvolvedor Front-end Júnior',
+    requisitos: ['JavaScript', 'GitHub', 'Lógica de programação'],
+    salario: 2800,
+    modalidade: 'Remoto',
+},
+{
+    id: 2,
+    empresa: 'CodeLab',
+    cargo: 'Estágio Front-end',
+    requisitos: ['JavaScript', 'Kamban', 'GitHub'],
+    salario: 1800,
+    modalidade: 'Presencial',
+},
+{
+    id: 3,
+    empresa: 'WebSolutions',
+    cargo: 'Programador JavaScript Júnior',
+    requisitos: ['JavaScript', 'Arrays', 'Objetos', 'Funções'],
+    salario: 3000,
+    modalidade: 'Presencial',
+},
+]
